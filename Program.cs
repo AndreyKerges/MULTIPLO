fb4f56719ea060595f3b91092ec84a5683319bd9
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-05-05 21:57:48 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int num, mult, resultado;
+
+
+Console.WriteLine("---MULTIPLO---");
+
+Console.WriteLine("Digite um número: ");
+num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Avaliar se é múltiplo de: ");
+mult = int.Parse(Console.ReadLine()!);
+
+resultado = mult * mult;
+if (resultado == num)
+{
+    Console.WriteLine($"{num} é múltiplo de {mult}");
+}
+
+else 
+{
+    Console.WriteLine($"{num} não é múltiplo de {mult}");
+}
